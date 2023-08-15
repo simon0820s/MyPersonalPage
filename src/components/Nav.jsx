28aaa14e082a496e2e9c3c8c7e9e0df2077887fc
simon0820s <simon0820s@gmail.com>
2023-08-14 20:07:14 -0500

    create nav for movile

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,9 +1,13 @@
 import { BiSolidHomeAlt2 } from "react-icons/bi";
+import { AiFillPhone } from "react-icons/ai";
+import {FaCode} from 'react-icons/fa'
 const Nav = () => {
   return (
-    <div className="bg-zinc-800 w-1/2 rounded-t-xl py-6 fixed bottom-0">
-      <div className="flex items-center justify-between px-4">
-        <BiSolidHomeAlt2 className="text-zinc-400" />
+    <div className="bg-zinc-800 w-3/4 rounded-t-full py-6 fixed bottom-0">
+      <div className="flex items-end justify-evenly jus px-6">
+        <AiFillPhone className="text-zinc-400 text-2xl" />
+        <BiSolidHomeAlt2 className="text-zinc-400 text-2xl" />
+        <FaCode className="text-zinc-400 text-2xl" />
       </div>
     </div>
   )
