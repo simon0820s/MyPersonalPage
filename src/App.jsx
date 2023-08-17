918d2f1c4fefa0d1f93090d43014fd9233ed3792
simon0820s <simon0820s@gmail.com>
2023-08-16 19:08:20 -0500

    create init

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Contact from './sections/Contact'
 
 const App = () => {
   return (
-    <div className='w-screen flex flex-col items-center p-4 bg-zinc-900 text-neutral-100'>
+    <div className='w-screen flex flex-col items-center bg-zinc-900 text-neutral-100'>
       <Nav />
       <Hero />
       <MyCode />
