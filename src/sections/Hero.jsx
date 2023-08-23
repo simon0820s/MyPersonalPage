41628327ff3e2d5ca1e579dff9ee3b38d44ad777
simon0820s <simon0820s@gmail.com>
2023-08-22 21:40:33 -0500

    create title and text to my code page

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -37,24 +37,20 @@ const Hero = () => {
           <h1 className="font-rubik text-xl text-purple-200">About Me</h1>
           <div className="flex gap-4 pl-2">
             <div className="flex flex-col items-center">
+              <span className="w-2 h-2 rounded-full bg-purple-200"></span>
               <motion.span
-                initial={{ opacity: 0, y: 80, scale: 0.2 }}
-                animate={{ opacity: 1, y: 0, scale: 1 }}
-                transition={{ duraction: 0.3 }}
-                className="w-2 h-2 rounded-full bg-purple-200"></motion.span>
-              <motion.span
-                initial={{ opacity: 0, y: 80, scale: 0.2 }}
+                initial={{ opacity: 0, y: -80, scale: 0.2 }}
                 animate={{ opacity: 1, y: 0, scale: 1 }}
                 transition={{ duraction: 0.3 }}
                 className="w-0.5 h-full bg-gradient-to-b from-purple-200"></motion.span>
             </div>
 
             <ul className="flex flex-col gap-1 text-xs text-zinc-400">
-              <li>* seedling I’m currently learning Next, Solidity, React, MongoDB</li>
-              <li>* All of my projects are available at <span className="font-bold text-purple-200 opacity-80">https://github.com/simon0820s</span>
+              <li><span className="text-purple-200">*</span> seedling I’m currently learning Next, Solidity, React, MongoDB</li>
+              <li><span className="text-purple-200">*</span> All of my projects are available at <span className="font-bold text-purple-200 opacity-80">https://github.com/simon0820s</span>
               </li>
-              <li>* Ask me about React, Next, ML, Blockchain, MongoDB</li>
-              <li>* How to reach me <span className="font-bold text-purple-200 opacity-80">simon0820s@gmail.com</span></li>
+              <li><span className="text-purple-200">*</span> Ask me about React, Next, ML, Blockchain, MongoDB</li>
+              <li><span className="text-purple-200">*</span> How to reach me <span className="font-bold text-purple-200 opacity-80">simon0820s@gmail.com</span></li>
             </ul>
           </div>
         </article>
--- src/sections/MyCode.jsx
+++ src/sections/MyCode.jsx
@@ -1,11 +1,14 @@
+/* eslint-disable react/no-unescaped-entities */
 function MyCode() {
   return (
-    <section 
+    <section
       id="myCode"
-      className="h-screen">
+      className="h-screen w-screen">
       <div className="flex flex-col items-center p-8">
-        <article>
-          <h1 className="text-3xl font-rubik text-purple-200">About my Code </h1>
+        <article className="flex flex-col items-center gap-4">
+          <h1 className="text-2xl font-rubik text-purple-200">About my Code </h1>
+          <p className="text-sm text-center opacity-80">I'm a highly organized programmer with a passion for modern technologies. My coding skills reflect not only technical prowess but also a dedication to applying best practices. With experience in crafting AI models and designing web pages, I bring versatility to the table. My commitment to staying updated with the latest trends ensures I remain at the forefront of innovation. By adhering to good programming practices, I create readable, maintainable code that demonstrates attention to detail. In all my projects, whether building AI models or developing websites, I prioritize excellence and optimization, showcasing adaptability and a holistic approach to problem-solving.
+          </p>
         </article>
       </div>
     </section>
