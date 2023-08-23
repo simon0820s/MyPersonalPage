a591ceacdb5b6a360a4f6ac07dfb8244cc89356f
simon0820s <simon0820s@gmail.com>
2023-08-22 21:52:21 -0500

    gap about me

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -24,7 +24,7 @@ const Hero = () => {
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#18181B" d="M0,0L26.7,26.7C53.3,53,107,107,160,149.3C213.3,192,267,224,320,224C373.3,224,427,192,480,197.3C533.3,203,587,245,640,250.7C693.3,256,747,224,800,218.7C853.3,213,907,235,960,250.7C1013.3,267,1067,277,1120,245.3C1173.3,213,1227,139,1280,96C1333.3,53,1387,43,1413,37.3L1440,32L1440,320L1413.3,320C1386.7,320,1333,320,1280,320C1226.7,320,1173,320,1120,320C1066.7,320,1013,320,960,320C906.7,320,853,320,800,320C746.7,320,693,320,640,320C586.7,320,533,320,480,320C426.7,320,373,320,320,320C266.7,320,213,320,160,320C106.7,320,53,320,27,320L0,320Z"></path></svg>
       </div>
       <div className="flex-2 flex flex-col gap-10 p-8">
-        <article className="flex flex-col items-start gap-2 text-3xl text-purple-200">
+        <article className="flex flex-col items-start gap-4 text-3xl text-purple-200">
           <div className="flex flex-col gap-1 font-rubik">
             <h1>I'm Web3</h1>
             <h1>Fullstack Ingineer</h1>
@@ -45,7 +45,7 @@ const Hero = () => {
                 className="w-0.5 h-full bg-gradient-to-b from-purple-200"></motion.span>
             </div>
 
-            <ul className="flex flex-col gap-1 text-xs text-zinc-400">
+            <ul className="flex flex-col gap-4 text-xs text-zinc-400">
               <li><span className="text-purple-200">*</span> seedling I’m currently learning Next, Solidity, React, MongoDB</li>
               <li><span className="text-purple-200">*</span> All of my projects are available at <span className="font-bold text-purple-200 opacity-80">https://github.com/simon0820s</span>
               </li>
--- src/sections/MyCode.jsx
+++ src/sections/MyCode.jsx
@@ -1,15 +1,19 @@
+import { BsGithub } from 'react-icons/bs'
 /* eslint-disable react/no-unescaped-entities */
 function MyCode() {
   return (
     <section
       id="myCode"
       className="h-screen w-screen">
-      <div className="flex flex-col items-center p-8">
+      <div className="flex flex-col gap-4 items-center p-8">
         <article className="flex flex-col items-center gap-4">
           <h1 className="text-2xl font-rubik text-purple-200">About my Code </h1>
           <p className="text-sm text-center opacity-80">I'm a highly organized programmer with a passion for modern technologies. My coding skills reflect not only technical prowess but also a dedication to applying best practices. With experience in crafting AI models and designing web pages, I bring versatility to the table. My commitment to staying updated with the latest trends ensures I remain at the forefront of innovation. By adhering to good programming practices, I create readable, maintainable code that demonstrates attention to detail. In all my projects, whether building AI models or developing websites, I prioritize excellence and optimization, showcasing adaptability and a holistic approach to problem-solving.
           </p>
         </article>
+        <a href='https://github.com/simon0820s' target='_blank' rel='noreferrer'>
+          <BsGithub className='text-zinc-400 text-5xl'/>
+        </a>
       </div>
     </section>
   )
