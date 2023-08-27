d1902187655c8acb1633ac52235a7abc9282b36f
simon0820s <simon0820s@gmail.com>
2023-08-26 20:18:46 -0500

    movile version ready

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ const App = () => {
       <Hero />
       <MyCode />
       <Contact />
-      <img className='fixed bottom-0 h-14 left-0 ml-4 opacity-80' src="./img/moonHero3.svg" alt="moonHero" />
+      <img className='fixed bottom-0 h-14 left-0 ml-4 opacity-70' src="./img/moonHero.svg" alt="moonHero" />
     </div>
   )
 }
