274c8f89febdd63e0c9d686cd273b45d249b19cb
simon0820s <simon0820s@gmail.com>
2023-08-27 20:20:50 -0500

    md responsive ready

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -8,13 +8,13 @@ function Contact() {
       className="bg-zinc-800 w-screen">
       <img src='./img/waveContact.svg' alt='waveContact' />
       <div className="z-10 absolute flex flex-col px-8 gap-2">
-        <h1 className="text-xl text-purple-200 font-rubik">About my contact</h1>
-        <p className="opacity-80 text-sm font-medium">you can contact me by the following means.</p>
+        <h1 className="text-xl sm:text-2xl text-purple-200 font-rubik">About my contact</h1>
+        <p className="opacity-80 text-sm sm:text-base font-medium">you can contact me by the following means.</p>
         <div className='z-10 flex items-center'>
           <span className='bg-purple-200 w-2 h-2 rounded-full' />
           <span className='w-3/4 h-0.5 bg-gradient-to-r from-purple-200' />
         </div>
-        <div className="flex mt-2 text-purple-200 text-2xl gap-2">
+        <div className="flex mt-2 text-purple-200 text-2xl sm:text-3xl gap-2">
           <motion.a
             initial={{ opacity: 0 }}
             whileInView={{ opacity: 1 }}
@@ -43,7 +43,7 @@ function Contact() {
             className='z-10'><BsFacebook /></motion.a>
         </div>
       </div>
-      <img className='h-56 opacity-70 float-right' src='./img/myContactHero.svg' />
+      <img className='h-56 sm:h-64 opacity-70 float-right' src='./img/myContactHero.svg' />
     </section>
   )
 }
