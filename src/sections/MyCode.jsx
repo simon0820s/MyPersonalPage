24d498cd240c424bc4874a1c40ffad47dab448de
simon0820s <simon0820s@gmail.com>
2023-11-26 22:52:17 -0500

    delete arrow import

--- src/sections/MyCode.jsx
+++ src/sections/MyCode.jsx
@@ -1,5 +1,4 @@
 import { BsGithub } from 'react-icons/bs'
-import { FaArrowRightLong } from 'react-icons/fa6'
 import { motion } from 'framer-motion'
 /* eslint-disable react/no-unescaped-entities */
 function MyCode() {
