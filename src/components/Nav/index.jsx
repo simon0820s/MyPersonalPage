22a640bb1320d0d3f0e6a65bb5ce79b5baf1cbbf
simon0820s <simon0820s@gmail.com>
2023-11-28 13:24:09 -0500

    creating animation nav buttons

--- src/components/Nav/index.jsx
+++ src/components/Nav/index.jsx
@@ -6,11 +6,13 @@ import './styles.css'
 const Nav = () => {
   return (
     <div className="z-50 h-16 flex justify-center fixed w-screen bottom-0">
+
       <img className='fixed bottom-0 h-14 sm:h-16 left-0 ml-2 opacity-70' src="../img/moonHero.svg" alt="moonHero" />
-      <div className="my-control bg-neutral-800 border-gray-700 border-x-2 border-t-1 h-full rounded-t-full flex items-center justify-center gap-3 sm:gap-10 px-10 sm:px-16 pt-2">
-        <a href="#contact"><AiFillPhone className="text-zinc-400 text-2xl sm:text-4xl hover:text-purple-300 transition-all ease-in-out duration-200" /> </a>
-        <a href="#hero"><BiSolidHomeAlt2 className="text-zinc-400 text-2xl sm:text-4xl hover:text-purple-300 transition-all ease-in-out duration-20" /> </a>
-        <a href="#myCode"><FaCode className="text-zinc-400 text-2xl sm:text-4xl hover:text-purple-300 transition-all ease-in-out duration-200" /> </a>
+
+      <div className="my-control flex items-center justify-center gap-3 sm:gap-10 bg-zinc-800 rounded-t-full border-gray-700 border-x-2 border-t-1 px-10 sm:px-16 pt-2 h-full">
+        <a href="#contact"><AiFillPhone className="text-zinc-400 text-2xl sm:text-4xl" /> </a>
+        <a href="#hero"><BiSolidHomeAlt2 className="text-zinc-400 text-2xl sm:text-4xl" /> </a>
+        <a href="#myCode"><FaCode className="text-zinc-400 text-2xl sm:text-4xl"/> </a>
       </div>
     </div>
   )
