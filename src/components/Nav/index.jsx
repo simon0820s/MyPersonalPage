5360c8d67a298ac6d1458064f41f3026993e8ec6
simon0820s <simon0820s@gmail.com>
2023-11-29 21:05:19 -0500

    animation ready

--- src/components/Nav/index.jsx
+++ src/components/Nav/index.jsx
@@ -2,18 +2,25 @@ import { BiSolidHomeAlt2 } from "react-icons/bi";
 import { AiFillPhone } from "react-icons/ai";
 import { FaCode } from 'react-icons/fa'
 import './styles.css'
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
 import { WebContext } from "../../Context";
 
 const Nav = () => {
 
   const context = useContext(WebContext)
 
+  const [controlClass, setControlClass] = useState('')
+
   useEffect(() => {
+    document.documentElement.style.setProperty('--animation-color', `#${context.principalColor}`)
+    setControlClass('magic-control')
+
     const delay = setTimeout(() => {
       document.documentElement.style.setProperty('--primary-color', `#${context.principalColor}`)
-    }, 300)
-    
+      setControlClass('')
+    }, 700)
+
+
     return () => clearTimeout(delay);
   }, [context])
 
@@ -22,7 +29,7 @@ const Nav = () => {
 
       <img className='fixed bottom-0 h-14 sm:h-16 left-0 ml-2 opacity-70' src="" alt="navHero" />
 
-      <div className="my-control flex items-center justify-center gap-3 sm:gap-10 bg-zinc-800 rounded-t-full border-gray-700 border-x-2 border-t-1 px-10 sm:px-16 pt-2 h-full">
+      <div className={`my-control ${controlClass} flex items-center justify-center gap-3 sm:gap-10 bg-zinc-800 rounded-t-full border-gray-700 border-x-2 border-t-1 px-10 sm:px-16 pt-2 h-full`}>
         <a onClick={() => context.setPrincipalColor('fe7272')} href="#contact"><AiFillPhone className="text-zinc-400 text-2xl sm:text-4xl" /> </a>
         <a onClick={() => context.setPrincipalColor('e9d5ff')} href="#hero"><BiSolidHomeAlt2 className="text-zinc-400 text-2xl sm:text-4xl" /> </a>
         <a onClick={() => context.setPrincipalColor('a1e0f7')} href="#myCode"><FaCode className="text-zinc-400 text-2xl sm:text-4xl" /> </a>
